722d9e4dc68f7cd903ce202398f2e922624734e6
Nitin71079 <fab.nitin718@gmail.com>
2026-08-08 21:54:19 +0530

    fix(writing): add missing React hook imports (useState, useEffect) to MockWriting.jsx

--- src/pages/MockWriting.jsx
+++ src/pages/MockWriting.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 import academicWritingTests from "../data/writing/academic/academicWritingTests";
